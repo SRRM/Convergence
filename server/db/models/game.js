8ec047c6a864311e72b833c5ce9e6034b16b1479
Shannon Cwikla <socwikla@gmail.com>
2018-02-05 12:01:01 -0600

    fixes randId in game model

--- server/db/models/game.js
+++ server/db/models/game.js
@@ -4,18 +4,19 @@ const db = require('../db')
 const Game = db.define('game', {
   randId: {
     type: Sequelize.STRING,
-    unique: true,
-    set() {
-      let val = Math.random().toString(36).slice(6)
-      this.setDataValue('randId', val)
-    }
+    defaultValue: function() {
+      return Math.random().toString(36).slice(6)
+    },
+    unique: true
   },
   personality: {
     type: Sequelize.STRING
   },
   status: {
     type: Sequelize.ENUM('Converged', 'Failed', 'Aborted')
-  },
+  }
 })
 
+
 module.exports = Game
+
